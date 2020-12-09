c43b4f592c8f173cd763fb826c9db99856153990
Vincent Viers <vincent.viers@beta.gouv.fr>
2020-12-09 14:45:16 +0100

    feat: update repo structure

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 import os
 
+from dotenv import load_dotenv, find_dotenv
+
+# find .env automagically by walking up directories until it's found
+dotenv_path = find_dotenv()
+# load up the entries as environment variables
+load_dotenv(dotenv_path)
+
 ENV = os.getenv("ENV", "develop")
