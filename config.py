856ad49e2f86a00efe02381515cff709ba02c327
Vincent Viers <vincent.viers@beta.gouv.fr>
2020-12-15 15:12:07 +0100

    config: mongodb params as config

--- config.py
+++ config.py
@@ -2,9 +2,16 @@ import os
 
 from dotenv import load_dotenv, find_dotenv
 
+from lib.utils import MongoParams
+
 # find .env automagically by walking up directories until it's found
 dotenv_path = find_dotenv()
 # load up the entries as environment variables
 load_dotenv(dotenv_path)
 
 ENV = os.getenv("ENV", "develop")
+MONGODB_PARAMS = MongoParams(
+    url=os.getenv("MONGO_URL", "mongodb://localhost"),
+    db=os.getenv("MONGO_DB", "prod"),
+    collection=os.getenv("MONGO_COLLECTION", "Features"),
+)
