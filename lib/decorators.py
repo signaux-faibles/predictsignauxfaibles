eef964b79dd634a02d6c6c3e1bdd581aceae2ab6
Vincent Viers <vincent.viers@beta.gouv.fr>
2021-02-16 11:29:53 +0100

    refacto: deal with env var in decorator itself

--- lib/decorators.py
+++ lib/decorators.py
@@ -1,19 +1,20 @@
 import logging
+import os
 from random import seed
 from typing import Callable
-
-from config import RANDOM_SEED
 
 
 def is_random(fun: Callable):
     """
     Functions annotated with @is_random will be seeded if the RANDOM_SEED env variable is found
     """
+    random_seed = os.getenv("RANDOM_SEED")
+    random_seed = int(random_seed) if random_seed is not None else False
 
     def wrapper(*args, **kwargs):
-        if RANDOM_SEED:
-            logging.debug(f"Seeding random function with seed {RANDOM_SEED}")
-            seed(RANDOM_SEED)
+        if random_seed:
+            logging.debug(f"Seeding random function with seed {random_seed}")
+            seed(random_seed)
         else:
             logging.debug("Random function is not seeded.")
         return fun(*args, **kwargs)
