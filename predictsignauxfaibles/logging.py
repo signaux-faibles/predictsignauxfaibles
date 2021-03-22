67d6a62b0303904c042492f69caa69ea489382c6
Vincent Viers <vincent.viers@beta.gouv.fr>
2021-03-22 15:04:13 +0100

    refacto: add debugging capabilities to SFDataset

---
+++ predictsignauxfaibles/logging.py
@@ -0,0 +1,28 @@
+import logging
+from pymongo import monitoring
+
+
+class CommandLogger(monitoring.CommandListener):
+    """
+    Log MongoDb Commands. Useful for debugging.
+    """
+
+    def started(self, event):
+        logging.debug(
+            f"Command {event.command_name} with request id"
+            f"{event.request_id} started on server {event.connection_id}"
+        )
+
+    def succeeded(self, event):
+        logging.debug(
+            f"Command {event.command_name} with request id "
+            f"{event.request_id} on server {event.connection_id} "
+            f"succeeded in {event.duration_micros} microseconds"
+        )
+
+    def failed(self, event):
+        logging.debug(
+            f"Command {event.command_name} with request id "
+            f"{event.request_id} on server {event.connection_id} "
+            f"failed in {event.duration_micros} microseconds"
+        )
