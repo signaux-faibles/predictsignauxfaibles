dc72cfc83a15ffc6b840975ae6e1eb169b6a1f4b
Vincent Viers <vincent.viers@beta.gouv.fr>
2021-03-23 17:50:07 +0100

    test: add remove_administrations test

--- tests/unit/preprocessors_test.py
+++ tests/unit/preprocessors_test.py
@@ -7,11 +7,17 @@ from predictsignauxfaibles.preprocessors import (
     acoss_make_avg_delta_dette_par_effectif,
     paydex_make_groups,
     paydex_make_yoy,
+    remove_administrations,
     run_pipeline,
     MissingDataError,
 )
 
-from tests.fake_data.dataframes import df_test_acoss, df_test_paydex, df_test_full
+from tests.fake_data.dataframes import (
+    df_test_acoss,
+    df_test_paydex,
+    df_test_full,
+    df_test_code_naf,
+)
 
 
 def test_final_pipeline():
@@ -30,14 +36,14 @@ def test_final_pipeline():
 def test_acoss_make_avg_delta_dette_par_effectif():
     data_in = df_test_acoss.copy()
     data_out = acoss_make_avg_delta_dette_par_effectif(data_in)
-    assert (data_out["avg_delta_dette_par_effectif"] == (0, 50, -0.04)).all()
+    assert (data_out["avg_delta_dette_par_effectif"] == [0, 50, -0.04]).all()
     assert len(data_out.columns) == len(df_test_acoss.columns) + 1
 
 
 def test_paydex_make_yoy():
     data_in = df_test_paydex.copy()
     data_out = paydex_make_yoy(data_in)
-    assert (data_out["paydex_yoy"] == (-81, 0, 9_985)).all()
+    assert (data_out["paydex_yoy"] == [-81, 0, 9_985]).all()
     assert len(data_out.columns) == len(df_test_paydex.columns) + 1
 
 
@@ -49,14 +55,21 @@ def test_paydex_make_group():
     assert len(data_out.columns) == len(df_test_paydex.columns) + 1
 
 
+def test_remove_administrations():
+    data_in = df_test_code_naf.copy()
+    data_out = remove_administrations(data_in)
+    assert (data_out["code_naf"] == ["A", "B", "E"]).all()
+    assert len(data_in) - len(data_out) == 2
+
+
 def test_full_pipeline_sucess():
     data_in = df_test_full.copy()
     data_out = run_pipeline(data_in, pipeline=PIPELINE)
     assert data_out.shape == (3, 11)
     # acoss
-    assert (data_out["avg_delta_dette_par_effectif"] == (0, 50, -0.04)).all()
+    assert (data_out["avg_delta_dette_par_effectif"] == [0, 50, -0.04]).all()
     # paydex yoy
-    assert (data_out["paydex_yoy"] == (-81, 0, 9_985)).all()
+    assert (data_out["paydex_yoy"] == [-81, 0, 9_985]).all()
     # paydex group
     expected = [pd.Interval(0, 15), pd.Interval(15, 30), pd.Interval(90, float("inf"))]
     assert (data_out["paydex_group"] == expected).all()
