44b1e7097ed6dacd96f6e9f3bd228522ebce3912
slebastard <lebastardsimon@gmail.com>
2021-04-13 17:43:48 +0200

    fix: importing vars from __main__ instead of whole module

--- tests/integration/test_modelrun_components.py
+++ tests/integration/test_modelrun_components.py
@@ -1,4 +1,4 @@
-import predictsignauxfaibles.__main__ as main
+from predictsignauxfaibles.__main__ import make_parser, ARGS_TO_ATTRS
 from predictsignauxfaibles.data import SFDataset, OversampledSFDataset
 
 
@@ -7,12 +7,12 @@ def test_args_to_attrs_consistency():
     Tests whether optionnal arguments that can be passed to run our package
     are correctly processed to configure our train/test/predict datasets
     """
-    args_directory = vars(main.make_parser().parse_args())
+    args_directory = vars(make_parser().parse_args())
     my_dataset = SFDataset()
     my_os_dataset = OversampledSFDataset(proportion_positive_class=0.3)
 
-    assert (main.ARGS_TO_ATTRS is not None) and isinstance(main.ARGS_TO_ATTRS, dict)
-    for arg, dataset_attr in main.ARGS_TO_ATTRS.items():
+    assert (ARGS_TO_ATTRS is not None) and isinstance(ARGS_TO_ATTRS, dict)
+    for arg, dataset_attr in ARGS_TO_ATTRS.items():
         assert arg in args_directory.keys()
         if dataset_attr[0] == "train":
             assert hasattr(my_os_dataset, dataset_attr[1])
