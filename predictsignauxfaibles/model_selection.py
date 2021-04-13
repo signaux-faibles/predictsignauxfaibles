480b0efec27e60249881ae47828deb09df14bc22
Vincent Viers <vincent.viers@beta.gouv.fr>
2021-04-13 14:24:14 +0200

    refacto: rename and update cv helper

--- predictsignauxfaibles/model_selection.py
+++ predictsignauxfaibles/model_selection.py
@@ -35,7 +35,7 @@ class SFModelEvaluator:
             A dictionary in the following format:
                 {"{fold_0}": score, ..., "{fold_num_folds}": score,}
         """
-        cv_splits = make_sf_test_validate_splits(
+        cv_splits = make_sf_train_test_splits(
             self.train_set.data, validate_set.data, num_folds
         )
 
@@ -67,15 +67,15 @@ class SFModelEvaluator:
 
 
 @is_random
-def make_sf_test_validate_splits(
-    train_data: pd.DataFrame, validate_data: pd.DataFrame, num_folds: int
+def make_sf_train_test_splits(
+    train_data: pd.DataFrame, test_data: pd.DataFrame, num_folds: int
 ) -> dict:
     """
     Implement Signaux Faibles' custom cross-validation strategy
 
-    Input:
+    Args:
         train_data: a DataFrame containing the training data
-        validate_data: a DataFrame containing the validation data
+        test_data: a DataFrame containing the testing data
         num_folds: the number of folds to make for cross-validation
 
     Return:
@@ -83,15 +83,15 @@ def make_sf_test_validate_splits(
             {
                 "{fold_number}": {
                     "train_on": [{indices}],
-                    "validate_on": [{indices}]
+                    "test_on": [{indices}]
                 }
             }
     """
 
-    if "siren" not in train_data.columns or "siren" not in validate_data.columns:
+    if "siren" not in train_data.columns or "siren" not in test_data.columns:
         raise ValueError("training data must contain a `siren` column")
 
-    if len(train_data) < num_folds or len(validate_data) < num_folds:
+    if len(train_data) < num_folds or len(test_data) < num_folds:
         raise ValueError("num_folds must be smaller than the size of the smallest set")
 
     if not isinstance(num_folds, int) or num_folds <= 0:
@@ -108,7 +108,6 @@ def make_sf_test_validate_splits(
     out = {}
     for i, siren in enumerate(siren_splits_train):
         out[i] = {"train_on": train_data[~train_data["siren"].isin(siren)].index}
-        out[i]["validate_on"] = validate_data[validate_data["siren"].isin(siren)].index
-        # TODO: remove "signaux-forts" from validate_on
+        out[i]["test_on"] = test_data[test_data["siren"].isin(siren)].index
 
     return out
