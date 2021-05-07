ddf54101bbd31f948c7b3f2b1829233b6b75d31b
slebastard <lebastardsimon@gmail.com>
2021-05-07 20:18:32 +0200

    feat: adapting test_args_to_attrs_consistency to explicit siret specification

--- tests/integration/test_modelrun_components.py
+++ tests/integration/test_modelrun_components.py
@@ -8,7 +8,7 @@ def test_args_to_attrs_consistency():
     are correctly processed to configure our train/test/predict datasets
     """
     args_directory = vars(make_parser().parse_args())
-    my_dataset = SFDataset()
+    my_dataset = SFDataset(sirets=[], sirens=[])
     my_os_dataset = OversampledSFDataset(proportion_positive_class=0.3)
 
     assert (ARGS_TO_ATTRS is not None) and isinstance(ARGS_TO_ATTRS, dict)
