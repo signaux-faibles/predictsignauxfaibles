d6b913b2813b972fa263d61afe7b8ec2f2b0bfa5
slebastard <lebastardsimon@gmail.com>
2021-05-11 13:05:37 +0200

    feat: adding functions to merge a list of predictions and produce lists

--- predictsignauxfaibles/model_selection.py
+++ predictsignauxfaibles/model_selection.py
@@ -1,8 +1,11 @@
+import logging
 import random
 
 import numpy as np
 import pandas as pd
 from predictsignauxfaibles.decorators import is_random
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level="INFO")
 
 
 @is_random
@@ -50,3 +53,66 @@ def make_sf_train_test_splits(
         out[i]["test_on"] = test_data[test_data["siren"].isin(siren)].index
 
     return out
+
+
+def merge_models(model_list: list):
+    # pylint: disable=all
+    # As pylint does not seem to handle recursions properly
+    """
+    Builds a single list of predicted probabilities based on several models,
+    listed by decreasing order of priority.
+    For a given SIRET, if no prediction is found for the first model in the list,
+    a prediction for the next model will be considered, etc
+    Arguments:
+        model_list: list
+            A list of pandas DataFrame containing, at least, the following columns:
+            - siren
+            - predicted_probability
+    """
+    merged = model_list.pop()
+    for model_id in range(len(model_list)):
+        model = model_list.pop()
+        merged = pd.merge(
+            model,
+            merged,
+            left_on="siret",
+            right_on="siret",
+            how="outer",
+            suffixes=("_main", "_supp"),
+        )
+        merged["predicted_probability"] = merged["predicted_probability_main"].fillna(
+            merged["predicted_probability_supp"]
+        )
+        merged = merged[["siret", "predicted_probability"]]
+    return merged
+
+
+def assign_flag(pred: float, t_rouge: float, t_orange: float):
+    """
+    Levels a risk score between 0 and 1 to a flag,
+    based on thresholds t_rouge and t_orange
+    """
+    if pred > t_rouge:
+        return "Alerte seuil F1"
+    elif pred > t_orange:
+        return "Alerte seuil F2"
+    return "Pas d'alerte"
+
+
+def split_predictions(preds: pd.DataFrame, t_rouge: float, t_orange: float):
+    """
+    Generates red/orange/green flags based on two thresholds,
+    and logs the share of entries per flag level
+    """
+    assert "predicted_probability" in preds.columns.tolist()
+    preds["alert"] = preds["predicted_probability"].apply(
+        lambda x: assign_flag(x, t_rouge, t_orange)
+    )
+
+    num_rouge = sum(preds["predicted_probability"] > t_rouge)
+    num_orange = sum(preds["predicted_probability"] > t_orange)
+    num_orange -= num_rouge
+    logging.info(f"{num_rouge} rouge ({round(num_rouge/preds.shape[0] * 100, 2)}%)")
+    logging.info(f"{num_orange} orange ({round(num_orange/preds.shape[0] * 100, 2)}%)")
+
+    return preds
