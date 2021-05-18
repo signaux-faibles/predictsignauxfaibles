d1b5c916c510d9f6e4ec20d0bc69bb9eeb65138c
Vincent Viers <vincent.viers@beta.gouv.fr>
2021-05-18 15:00:06 +0200

    fix: fix bug when fields is None

--- predictsignauxfaibles/data.py
+++ predictsignauxfaibles/data.py
@@ -142,13 +142,14 @@ class SFDataset:
             cursor.close()
 
         # create and fill missing fields with NAs
-        if not set(self.fields).issubset(set(self.data.columns)):
-            missing = set(self.fields) - set(self.data.columns)
-            logging.info(
-                f"Creating missing columns {missing} and filling them with NAs."
-            )
-            for feat in missing:
-                self.data[feat] = NAN
+        if self.fields is not None:
+            if not set(self.fields).issubset(set(self.data.columns)):
+                missing = set(self.fields) - set(self.data.columns)
+                logging.info(
+                    f"Creating missing columns {missing} and filling them with NAs."
+                )
+                for feat in missing:
+                    self.data[feat] = NAN
 
         return self
 
