5aaa456970e3a184ab87f559b5718d06c3c4722f
slebastard <lebastardsimon@gmail.com>
2021-06-02 15:01:48 +0200

    feat: upgrading `predictsignauxfaibles.merge_models`
    1. Function `merge_models` now takes as input:
    - a list of model predictions as named tuples with fields "name" (str) and "predictions", a pandas.DataFrame containing at least columns "siret" and "alert"
    - a list of columns to be merged from the list of models
    The model predictions must be provided in decreasing order of priority, meaning that for each record, model predictions found earlier in the list are given priority
    The function returns a merged dataframe of model predictions, containing column "siret" (used as key for merging)  and the list of columns specified
    
    2. Updating notebook `04-merge_models_into_list.ipynb` to make it a tutorial to produced a unified list from a set of model predictions, integrating a pipeline of Redressements

--- predictsignauxfaibles/merge_models.py
+++ predictsignauxfaibles/merge_models.py
@@ -1,11 +1,46 @@
 # pylint: disable=all
+from collections import namedtuple
 import logging
 from typing import List
 
 import pandas as pd
 
+ModelPreds = namedtuple("ModelPreds", ["name", "predictions"])
 
-def merge_models(model_list: List[pd.DataFrame]):
+
+def merge_columns(
+    df_main: pd.DataFrame, df_supp: pd.DataFrame, col_names: str, supp_model_name: str
+):
+    suffixes = ("_main", "_supp")
+    merged = pd.merge(
+        df_main,
+        df_supp,
+        left_on="siret",
+        right_on="siret",
+        how="outer",
+        suffixes=suffixes,
+        indicator=True,
+    )
+    assert "_merge" in merged.columns, "'_merge' indicator cannot be found in DataFrame"
+    merged.loc[merged["_merge"] == "right_only", "which_model"] = supp_model_name
+
+    for col_name in col_names:
+        assert (
+            col_name + suffixes[0] in merged.columns
+        ), f"{col_name+suffixes[0]} cannot be found in DataFrame"
+        assert (
+            col_name + suffixes[1] in merged.columns
+        ), f"{col_name+suffixes[1]} cannot be found in DataFrame"
+
+        merged[col_name] = merged[col_name + suffixes[0]]
+        merged.loc[merged["_merge"] == "right_only", col_name] = merged.loc[
+            merged["_merge"] == "right_only", col_name + suffixes[1]
+        ]
+
+    return merged[["siret", "which_model"] + col_names]
+
+
+def merge_models(model_list: List[namedtuple], cols_to_merge: List[str]):
     """
     Builds a single list of predicted probabilities based on several models,
     listed by decreasing order of priority.
@@ -16,7 +51,9 @@ def merge_models(model_list: List[pd.DataFrame]):
             A list of pandas DataFrame containing, at least, the following columns: siren, predicted_probability
     """
     try:
-        merged = model_list.pop()
+        main_model = model_list.pop(0)
+        merged = main_model.predictions
+        merged["which_model"] = main_model.name
     except IndexError:
         logging.error("model_list appears to be empty")
     if model_list == []:
@@ -24,16 +61,11 @@ def merge_models(model_list: List[pd.DataFrame]):
         return merged
 
     for model in model_list:
-        merged = pd.merge(
-            model,
-            merged,
-            left_on="siret",
-            right_on="siret",
-            how="outer",
-            suffixes=("_main", "_supp"),
+        merged = merge_columns(
+            df_main=merged,
+            df_supp=model.predictions,
+            col_names=cols_to_merge,
+            supp_model_name=model.name,
         )
-        merged["predicted_probability"] = merged["predicted_probability_main"].fillna(
-            merged["predicted_probability_supp"]
-        )
-        merged = merged[["siret", "predicted_probability"]]
+
     return merged
